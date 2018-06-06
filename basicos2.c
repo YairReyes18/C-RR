99d0b1ab16f0aa35a398fe2be1ab9f4627887dfc
Yair_GL <azaelyair@hotmail.com>
2018-06-05 22:37:46 -0500

    basics

---
+++ basicos2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	char dicc[5][16] = {"hola", "ahi", "ay", "jaja xd :v"};
+	char *vptr[5];
+	char *vptr2[]={NULL,NULL,NULL,NULL,NULL};
+	char *vptr3[10]={NULL};
+	int a = 123456789;
+	char *p=(char *)(&a); //casting para poder accesar a los datos
+	int i;
+
+	
+	for( i = 0; dicc[i][0] != '\0'; i++ )
+		puts(dicc[i]);
+
+	for(i=0; i<5; i++){
+		vptr[i] = dicc[i];
+		printf("%p %p %s \n", dicc[i], vptr[i], vptr[i] );
+
+	}
+
+	//little endian / big endian
+	printf("%x %x %x %x\n", *(p), *(p+1), *(p+2), *(p+3) );
+
+}
+
